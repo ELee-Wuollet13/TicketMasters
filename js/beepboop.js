1351e8574d4d0b8f9b13593067889ea770401cbd
Student <example@example.com>
2019-11-01 13:25:27 -0700

    Finished bugfixing. Code works (finally).

--- js/beepboop.js
+++ js/beepboop.js
@@ -1,33 +1,32 @@
 $(document).ready(function() {
-
-  var beeps=['','Beep!','Boop!',"I'm sorry, Dave. I'm afraid I can't do that."];
-
   $("form#boopform").submit(function(event) {
-    var num = parseInt($("input#num").val());
+    var beeps=['',' Beep! ',' Boop! '," I'm sorry, Dave. I'm afraid I can't do that. "];
+
+    var num = $("#num").val();
+  
     var range = numRange(num);
     console.log(range);
-    num = toString(num);
+
     for (var i=0;i<num;i++){
-      var buup = beeps[boopQuery(num[i])];
+      var buup = beeps[boopQuery(range[i])];
       if(buup) {
         range[i]=buup;
       }
     }
-    $("#maxbooped").text(num);
+    var output = range.join(' ');
+    $("#maxbooped").text(output);
     event.preventDefault();
-  })
+  });
+
 });
 function numRange(num){
-  console.log(num);
   var range=[];
   for (var i=0; i<=num;i++){
     range.push(i);
   } // for
-  console.log(range);
   return range;
 } // function numRange
 function boopQuery(num){
-  console.log("Query ran!");
   num = num.toString(10);
   biip=0;
   for (var i=num.length; i>-1;i--){
