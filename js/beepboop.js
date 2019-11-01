ebd6f4c15d471ceb031b1daaef63c7f90d712e4c
Student <example@example.com>
2019-11-01 11:20:49 -0700

    Completely forgot to commit xD Been working on input/output. Getting some errors for no discernable reason, been bugfixing.

--- js/beepboop.js
+++ js/beepboop.js
@@ -1,25 +1,33 @@
 $(document).ready(function() {
-  var beeps=['','Beep!','Boop!',"I'm sorry, Dave. I'm afraid I can't do that."];
-  var num=5; // This is a pretend input. Make it an input later!!!!!
-  var range = numRange(num);
-  console.log(range);
 
-  for (var i=0;i<num;i++){
-    var buup = beeps[boopQuery(range[i])];
-    if(buup) {
-      range[i]=buup;
+  var beeps=['','Beep!','Boop!',"I'm sorry, Dave. I'm afraid I can't do that."];
+
+  $("form#boopform").submit(function(event) {
+    var num = parseInt($("input#num").val());
+    var range = numRange(num);
+    console.log(range);
+    num = toString(num);
+    for (var i=0;i<num;i++){
+      var buup = beeps[boopQuery(num[i])];
+      if(buup) {
+        range[i]=buup;
+      }
     }
-  }
-  console.log(range);
+    $("#maxbooped").text(num);
+    event.preventDefault();
+  })
 });
 function numRange(num){
+  console.log(num);
   var range=[];
   for (var i=0; i<=num;i++){
     range.push(i);
   } // for
+  console.log(range);
   return range;
 } // function numRange
 function boopQuery(num){
+  console.log("Query ran!");
   num = num.toString(10);
   biip=0;
   for (var i=num.length; i>-1;i--){
