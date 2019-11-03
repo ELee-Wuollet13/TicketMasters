ad9838d1b5b7558c74c8911b97ddeb3921140ea8
lillianTheuma <definatelyruby@gmail.com>
2019-11-02 17:20:13 -0700

    Updated submit function to use roman numerals, will add a button to swap between the two in a later update

--- js/beepboop.js
+++ js/beepboop.js
@@ -15,6 +15,12 @@ $(document).ready(function() {
         range[i]=buup;
       }
     }
+    // Replaces numbers with roman numerals
+    for (var i=0; i<range.length;i++){
+      if (((range[i])/(range[i])) === 1) {
+        range[i] = toRoman(range[i].toString());
+      }
+    }
 
     var output = range.join(' ');
     $("#maxbooped").text(output);
@@ -45,4 +51,22 @@ function boopQuery(num){
     }
   } // for loop
   return biip;
+}
+
+function toRoman(num) {
+
+  num = num.replace( /[^0-9]/g, '' );
+
+  if (!(parseInt(num)/parseInt(num)===1)) {
+    return [''];
+  }
+  var chars = [['','I','II','III','IV','V','VI','VII','VIII','IX'],['','X','XX','XXX','XL','L','LX','LXX','LXXX','XC'],['','C','CC','CCC','CD','D','DC','DCC','DCCC','CM'],['','M','MM','MMM','MMMM','MMMMM','MMMMMM','MMMMMM','MMMMMMM','MMMMMMMM','MMMMMMMMM']]
+  var output=[];
+  var j = 0;
+  for (var i=((num.length)-1); i>-1;i--){
+    output.push(chars[i][num[j]]);
+    j+=1;
+  }
+  output = output.join('');
+  return output;
 }
